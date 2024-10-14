af53c355724ad18dc0d57ed8308c6bf7ad4f5443
MrNurbek1223 <xamrayevnurbek00@gmail.com>
2024-10-14 18:11:03 +0500

    asdasd

--- qozon/settings.py
+++ qozon/settings.py
@@ -67,6 +67,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
 SIMPLE_JWT = {
