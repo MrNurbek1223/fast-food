10dc1787141a1e780b769242b38edbd984ffaa69
MrNurbek1223 <xamrayevnurbek00@gmail.com>
2024-10-15 10:58:35 +0500

    asdasd

--- qozon/urls.py
+++ qozon/urls.py
@@ -23,8 +23,8 @@ schema_view = get_schema_view(
         default_version="v1",
     ),
     public=True,
-    permission_classes=[permissions.AllowAny,],
-    authentication_classes=[JWTAuthentication,],
+    permission_classes=[permissions.AllowAny, ],
+    authentication_classes=[JWTAuthentication, ],
 )
 
 router = DefaultRouter()
@@ -46,5 +46,9 @@ urlpatterns = [
                           name='schema-json'),
                   path('swagger1/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
                   path('redoc1/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+                  path('api/docs<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+                  path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                  path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
                                                                                            document_root=settings.MEDIA_ROOT)
