a4558db7014dfa195c65e1888ee140a02601656d
MrNurbek1223 <xamrayevnurbek00@gmail.com>
2024-10-15 10:52:22 +0500

    asdasd

--- qozon/urls.py
+++ qozon/urls.py
@@ -15,18 +15,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.urls import path, re_path
 from rest_framework_simplejwt.authentication import JWTAuthentication
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title="Fast Food API",
-#         default_version='v1',
-#         description="Test description",
-#         terms_of_service="https://www.google.com/policies/terms/",
-#         contact=openapi.Contact(email="support@fastfood.uz"),
-#         license=openapi.License(name="BSD License"),
-#     ),
-#     public=True,
-#     permission_classes=(permissions.AllowAny,),
-# )
+
 schema_view = get_schema_view(
     openapi.Info(
         title='Chat API',
@@ -55,7 +44,7 @@ urlpatterns = [
                   path('', include(router.urls)),
                   re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
                           name='schema-json'),
-                  path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-                  path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+                  path('swagger1/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                  path('redoc1/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
                                                                                            document_root=settings.MEDIA_ROOT)
